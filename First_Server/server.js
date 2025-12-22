774b73a04b0761e4c9b0f8cb6190e1958cecb9ff
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2025-12-23 00:29:38 +0530

    Learning Backend Operations using NodeJs

--- First_Server/server.js
+++ First_Server/server.js
@@ -1,8 +1,20 @@
+// this file creates a simple HTTP server using Node.js and sends a basic HTML response
+
 const http = require('http');
 
 
 const server = http.createServer((req, res) => {
+
+    // Log the request details to the console
     console.log(req.url, req.method, req.headers);
+
+    // Set the response header and send a simple HTML response
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Server</title></head>')
+    res.write('<body><h1>Hello from my first server!</h1></body>')
+    res.write('</html>')
+    res.end();
 })
 
 const PORT = 4000;
