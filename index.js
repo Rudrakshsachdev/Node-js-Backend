552379da751572c0815199edc1a7535f7e1f3a69
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2025-12-22 22:35:21 +0530

    Learning Backend Operations using NodeJs

---
+++ index.js
@@ -0,0 +1,12 @@
+// Simple console log and file operation in Node.js
+console.log('Hello, World! Learning Node.js is fun!');
+
+
+
+const fs = require('fs');
+
+fs.writeFile("output.txt", "Performing file operations in Node.js", (err) => {
+    if (err) throw err;
+    console.log("File has been created and data written to it.");
+});
+
