a197f719e1b3bc991b7b34ffb37ca7c7930dd716
Rudrakshsachdev <rudrakshsachdeva04@gmail.com>
2026-07-13 00:03:56 +0530

    feat: implement MongoDB connection and server startup logic

--- expense-tracker/Backend/src/app.js
+++ expense-tracker/Backend/src/app.js
@@ -4,4 +4,8 @@ const express =  require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Welcome to Expense Tracker Application");
+})
+
 module.exports = app;
--- expense-tracker/Backend/src/server.js
+++ expense-tracker/Backend/src/server.js
@@ -1,9 +1,21 @@
 // this file is for running the node js application
 
+const dotenv = require("dotenv").config();
+
 const app = require("./app");
+const { connectToMongoDB } = require("./config/Database");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function startServer() {
+  await connectToMongoDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer().catch((error) => {
+  console.error("Failed to start server:", error);
+  process.exit(1);
 });
